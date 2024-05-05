836e6d6c95e3de6247406c02bd613c661d565311
Theofilos Intzoglou <int.teo@gmail.com>
2024-05-05 15:24:38 +0000

    Προσθήκη αντίστοιχων ελέγχων και για εκτός laravel

--- tests/Feature/Non-Laravel/RBACTest.php
+++ tests/Feature/Non-Laravel/RBACTest.php
@@ -90,3 +90,62 @@ test('RBAC can get parents of a role', function () {
         ->toContainOnlyInstancesOf(Role::class);
     expect($parents->where('id', 4))->toHaveCount(0);
 });
+
+test('RBAC can delete a leaf of the role tree', function () {
+    $rbac = new RBAC([
+        'driver' => 'sqlite',
+        'database' => ':memory:',
+    ]);
+    $rbac->loadJsonFile(__DIR__.'/../../Data/Json/Dataset.json');
+
+    /** @var Role $role */
+    $role = Role::find(4);
+    $role->delete();
+    expect(Role::find(4))->toBeNull();
+    expect(Role::find(1)->children())
+        ->toHaveCount(2);
+    expect(Role::find(1)->children()->where('id', 4))->toHaveCount(0);
+});
+
+test('RBAC can delete the root of the role tree', function () {
+    $rbac = new RBAC([
+        'driver' => 'sqlite',
+        'database' => ':memory:',
+    ]);
+    $rbac->loadJsonFile(__DIR__.'/../../Data/Json/Dataset.json');
+
+    /** @var Role $role */
+    $role = Role::find(1);
+    $role->delete();
+    expect(Role::all())->toHaveCount(0);
+});
+
+test('RBAC can delete a role from the middle of the tree', function () {
+    $rbac = new RBAC([
+        'driver' => 'sqlite',
+        'database' => ':memory:',
+    ]);
+    $rbac->loadJsonFile(__DIR__.'/../../Data/Json/Dataset.json');
+
+    /** @var Role $role */
+    $role = Role::find(3);
+    $role->delete();
+    expect(Role::all())->toHaveCount(2);
+    expect(Role::all()->whereIn('id', [1, 2]))->toHaveCount(2);
+    expect(Role::find(1)->children())->toHaveCount(1);
+});
+
+test('RBAC can move a role from the middle of the tree', function () {
+    $rbac = new RBAC([
+        'driver' => 'sqlite',
+        'database' => ':memory:',
+    ]);
+    $rbac->loadJsonFile(__DIR__.'/../../Data/Json/Dataset.json');
+
+    /** @var Role $role */
+    $role = Role::find(3);
+    $role->parent_id = 2;
+    $role->save();
+    expect(Role::all())->toHaveCount(4);
+    expect(Role::find(4)->parents())->toHaveCount(3);
+});
