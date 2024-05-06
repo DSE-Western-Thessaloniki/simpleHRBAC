735b60a24e63e22cf367c0382b09bf8290d32d07
Theofilos Intzoglou <int.teo@gmail.com>
2024-05-06 13:46:56 +0000

    Προσθήκη ελέγχων κατά τη δημιουργία και ενημέρωση κόμβων

--- src/Observers/RoleObserver.php
+++ src/Observers/RoleObserver.php
@@ -2,16 +2,49 @@
 
 namespace Dsewth\SimpleHRBAC\Observers;
 
+use Dsewth\SimpleHRBAC\Exceptions\RBACException;
 use Dsewth\SimpleHRBAC\Models\Role;
 
 class RoleObserver
 {
+    public function creating(Role $role): void
+    {
+        // Έλεγξε αν υπάρχει άλλος κόμβος χωρίς γονέα
+        if ($role->parent_id === null && Role::where('parent_id', null)->count() === 1) {
+            throw new RBACException('Only the root node can have null parent_id');
+        }
+    }
+
     /**
      * Handle the Role "created" event.
      */
     public function created(Role $role): void
     {
+        if ($role->parent_id !== null && $role->parent_id === $role->id) {
+            $role->delete();
+            throw new RBACException('A node cannot be its own parent');
+        }
+
         $role->tree()->addToTree();
+    }
+
+    public function updating(Role $role): void
+    {
+        if (! $role->isDirty('parent_id')) {
+            return;
+        }
+
+        if ($role->parent_id === null) {
+            throw new RBACException('Only the root node can have null parent_id');
+        }
+
+        if ($role->parent_id === $role->id) {
+            throw new RBACException('A node cannot be its own parent');
+        }
+
+        if (Role::where('parent_id', null)->first()->id === $role->id) {
+            throw new RBACException('You cannot move root node');
+        }
     }
 
     /**
@@ -24,6 +57,13 @@ class RoleObserver
         }
 
         $role->tree()->moveNode();
+    }
+
+    public function deleting(Role $role): void
+    {
+        if ($role->parent_id === null) {
+            throw new RBACException('The root node cannot be removed from the tree');
+        }
     }
 
     /**
