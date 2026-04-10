82a168096516f7492e4b6c1529dd14a4ada08057
Theofilos Intzoglou <int.teo@gmail.com>
2026-04-10 18:25:37 +0000

    Ενημέρωση docblock

--- src/Facades/RBAC.php
+++ src/Facades/RBAC.php
@@ -6,10 +6,12 @@ use Dsewth\SimpleHRBAC\Services\RBACService;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static bool importJsonFile(string $filename)
- * @method static bool importData(array $data)
- * @method static \Illuminate\Support\Collection<\Dsewth\SimpleHRBAC\Models\Permission> getPermissionsOf($user)
+ * @method static \Illuminate\Support\Collection<\Dsewth\SimpleHRBAC\Models\Permission> getPermissionsOf(\Illuminate\Database\Eloquent\Model $user)
  * @method static bool can(int $userId, string $permission)
+ * @method static void invalidateUserCache(int $userId)
+ * @method static void invalidatePermissionCache(string $permission)
+ * @method static void invalidateAllCache()
+ * @method static \Illuminate\Support\Collection<\Illuminate\Database\Eloquent\Model> getUsersWithPermission(string $permission)
  */
 class RBAC extends Facade
 {
